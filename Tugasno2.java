b4228dd6faa784014c9e275330e38f085b66994a
muiddd <muidabdulmuid99@gmail.com>
2024-09-26 21:59:44 +0700

    commit akhir (maaf bu vivi baru lihat lagi ada yang salah)

--- Tugasno2.java
+++ Tugasno2.java
@@ -10,11 +10,11 @@ public class Tugasno2 {
 
         if (usia >= 0 && usia <= 12){
             kategori = "Anak";
-        } else if (usia >= 13){
+        } else if (usia >= 13 && usia <= 19){
             kategori = "Remaja";
-        } else if (usia >= 20){
+        } else if (usia >= 20 && usia <= 64){
             kategori = "Dewasa";
-        } else if (usia >= 64){
+        } else if (usia >= 65){
             kategori = "Lansia";
         } else {
             kategori = "Tidak masuk akal";
